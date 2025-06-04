9fe6c82e0bf8f2e1d9d4d2293db3a2a1a3b78a82
salisspro <salisspro@gmail.com>
2025-06-04 21:06:43 +0100

    new changes

--- public/components/Hero.jsx
+++ public/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
 
             <div className='bg-gray-100 min-h-screen flex flex-col items-center justify-center text-center md:grid md:grid-cols-2'>
 
-                <h1 className='text-4xl text-gray-900 md:text-5xl md:font-bold mt-20'>Welcome to Our Website</h1>
+                <h1 className='text-4xl text-gray-900 md:text-5xl md:font-bold mt-30'>Welcome to Our Website</h1>
 
                 <div className='flex ' >
                     <p className='mt-4 text-lg text-gray-600 mx-10 mb-10'>We are glad to have you here. Explore our content and enjoy your stay!</p>
