3d51d208bae5a2d87b947385fe7b87bcf846bb6f
salisspro <salisspro@gmail.com>
2025-06-11 22:41:26 +0100

    this is a sample commit message

--- public/components/Hero.jsx
+++ public/components/Hero.jsx
@@ -7,7 +7,8 @@ export default function Hero() {
             <div className='bg-gray-100 min-h-screen flex flex-col items-center justify-center text-center md:grid md:grid-cols-2'>
 
                 <h1 className='text-4xl text-gray-900 md:text-5xl md:font-bold mt-30'>Welcome to Our Website</h1>
-
+                <h3 className='text-2xl text-gray-700 md:text-3xl md:font-semibold'>This website is all about contractors services</h3>
+                <h2 className='text-xl text-gray-600 md:text-2xl md:font-medium'>salisspro</h2>
                 <div className='flex ' >
                     <p className='mt-4 text-lg text-gray-600 mx-10 mb-10'>We are glad to have you here. Explore our content and enjoy your stay!</p>
                 </div>
@@ -15,7 +16,7 @@ export default function Hero() {
                 <div>
                     <img src={IMG} className='w-full h-auto' />
 
-                    <button className='border py-3 rounded-lg hover:bg-yellow-500 transition duration-1000 mt-5 w-[20em]'>Get Started</button>
+                    <button className='border py-3 rounded-lg hover:bg-yellow-500 transition duration-1000 mt-5 w-[20em]'>Get Started for free</button>
 
                 </div>
             </div>
